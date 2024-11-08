be379f66afbe80264babe99d3d15f77a67b2ee03
CocoisBuggy <g.colinharley@gmail.com>
2024-11-08 15:32:24 +0200

    fix for #409
    added a new suite of tests for asserting area behaviors at the data model level.

--- src/model/__tests__/AreaUtils.ts
+++ src/model/__tests__/AreaUtils.ts
@@ -1,12 +1,4 @@
-// import muid from 'uuid-mongodb'
-import { genMUIDFromPaths } from '../MutableAreaDataSource.js'
-
 describe('Test area utilities', () => {
-  it('generates UUID from area tokens', () => {
-    const paths = ['USA', 'Red Rocks']
-    const uid = genMUIDFromPaths(paths, 'Calico Basin')
-
-    expect(uid.toUUID().toString()).toEqual('9fbc30ab-82d7-5d74-85b1-9bec5ee00388')
-    expect(paths).toHaveLength(2) // make sure we're not changing the input!
-  })
+  test.todo('The name comparison code unit')
+  test.todo('The name-uniqueness system with other side-effects stripped out')
 })
