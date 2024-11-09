e0c154564ea0b06bd50daca463660adbdb154f88
CocoisBuggy <g.colinharley@gmail.com>
2024-11-09 14:31:04 +0200

    Allow compass to open on the correct connection
    Previously you would need to re-enter the auth credentials.

--- shell.nix
+++ shell.nix
@@ -12,6 +12,7 @@ pkgs.mkShell {
     mongodb-ce
     mongodb-compass
     mongosh
+    gsettings-desktop-schemas
   ];
 
   # MONGOMS_DOWNLOAD_URL = "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2404-8.0.1.tgz";
@@ -24,13 +25,17 @@ pkgs.mkShell {
   MONGOMS_VERSION = "7.0.14";
 
   shellHook = ''
+
     set -a
     source .env
+    mongo_cnx="$MONGO_SCHEME://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@$MONGO_SERVICE/$MONGO_DBNAME?authSource=$MONGO_AUTHDB&tls=$MONGO_TLS"
 
     # mongotop alias
-    alias mto="mongotop --uri=\"$MONGO_SCHEME://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@$MONGO_SERVICE/$MONGO_DBNAME?authSource=$MONGO_AUTHDB&tls=$MONGO_TLS\""
+    alias mto="mongotop --uri=$mongo_cnx"
     # mongostat alias
-    alias mst="mongostat --uri=\"$MONGO_SCHEME://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@$MONGO_SERVICE/$MONGO_DBNAME?authSource=$MONGO_AUTHDB&tls=$MONGO_TLS\""
+    alias mst="mongostat --uri=$mongo_cnx"
+    # Compass tooling
+    alias compass="mongodb-compass --theme=dark $mongo_cnx"
 
     echo "🧗 Alle!"
   '';
