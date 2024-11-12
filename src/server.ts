2f108b72c1e43355e7fe5ebeed8ed8d235377c02
Viet Nguyen <3805254+vnugent@users.noreply.github.com>
2024-11-12 10:03:17 -0800

    fix: increase payload limit (#421)

--- src/server.ts
+++ src/server.ts
@@ -55,7 +55,13 @@ export async function createServer (): Promise<{ app: express.Application, serve
   })
   // server must be started before applying middleware
   await server.start()
-  server.applyMiddleware({ app, path: '/' })
+  server.applyMiddleware({
+    app,
+    path: '/',
+    bodyParserConfig: {
+      limit: '10mb'
+    }
+  })
 
   return { app, server }
 }
